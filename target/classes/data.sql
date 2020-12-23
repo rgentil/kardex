d3fdce12233194210840dc98d132cb5d3579c4ea
Betina Haak <betina.haak@3sharecorp.com>
2020-12-23 16:18:10 -0300

    removed more target

--- target/classes/data.sql
+++
@@ -1,4 +0,0 @@
-insert into Stock(name, current_price, last_updated_at,CURRENT_AMOUNT)values('Vaso Batman', 178.89, NOW(),1);
-insert into Stock(name, current_price, last_updated_at,CURRENT_AMOUNT)values('Camiseta Wonder woman', 160.84, NOW(),1);
-insert into Stock(name, current_price, last_updated_at,CURRENT_AMOUNT)values('Accesorio Flash', 179.98, NOW(),1);
-insert into Stock(name, current_price, last_updated_at,CURRENT_AMOUNT)values('Vaso Flash', 185.23, NOW(),1);
